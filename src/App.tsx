b27a139699d29d3b92294661140036f26312946b
Erik Martus <erik@emartus.dev>
2021-12-02 16:59:31 -0500

    :tada: Initialize project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="font-sans">
+      <h1>Windmaker</h1>
+      <p>A TailwindCSS configuration tool.</p>
+    </div>
+  );
+}
+
+export default App;
